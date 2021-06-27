9fff88a9fe776fc9fec18ffd9ab35b1c0f6f81b6
Hwan <dnfwlq8054@naver.com>
2021-06-27 22:01:47 +0900

    update

--- Array/append.go
+++ Array/append.go
@@ -3,6 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	arr := []int{1, 2, 3, 4, 5}
+	arr2 := []int{10, 20, 30}
 
-	fmt.Println("hi")
+	arr = append(arr, 100)               // output : [1, 2, 3, 4, 5, 100]
+	arr3 := append(arr, arr2[0], arr[1]) // output : [1, 2, 3, 4, 5, 100, 10, 20]
+	arr4 := append(arr, arr2...)         // output : [1, 2, 3, 4, 5, 100, 10, 20, 30]
+	arr5 := append(arr[:2], arr[3:]...)  // output : [1, 2, 4, 5, 100]
+	fmt.Println(arr)                     //output : [1, 2, 4, 5, 100, 100]
 }
